51a352412e7999445d7afced3c907b344f4c16c7
Mrlolpro <leonardoollero14@gmail.com>
2023-01-01 17:07:51 +0100

    fix: more wait

--- src/modules/loadbalancer.js
+++ src/modules/loadbalancer.js
@@ -107,7 +107,7 @@ async function initTokens() {
   var tokens = await getTokens();
   for (var i = 0; i < tokens.length; i++) {
     var token = tokens[i];
-    await delay(1000);
+    await delay(5000);
     await initChat(token.email, token.password, token.id);
   }
 }
