abff4eae739a9ea27c66b9ae21c9bf916aa7eafe
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-01-16 16:50:19 +0100

    fix: import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ShardingManager } from "discord.js";
 import "dotenv/config";
-import { initTokens } from "./modules/loadbalancer";
+import { initTokens } from "./modules/loadbalancer.js";
 
 const manager = new ShardingManager("./dist/bot.js", {
   token: process.env.TOKEN,
