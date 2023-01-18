d33f88ed415fd8fd0c24b4414df5ba1965d0dfdc
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-01-18 20:21:10 +0100

    fix: guild

--- src/modules/features.ts
+++ src/modules/features.ts
@@ -1,9 +1,11 @@
 import { Client } from "discord.js";
 export async function checkIsTuring(client: Client, userId: string) {
   var guild = await client.guilds.cache.get("899761438996963349");
-  var member = await guild.members.cache.get(userId);
-  if (member) {
-    return true;
+  if (guild) {
+    var member = await guild.members.cache.get(userId);
+    if (member) {
+      return true;
+    }
   }
   return false;
 }
