23c09844965e17d8e7dda9abaa93e5415583f03a
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-01-28 15:43:08 +0100

    fix: load balancer

--- src/modules/sonic.ts
+++ src/modules/sonic.ts
@@ -1,8 +1,27 @@
 import api from "api";
+import supabase from "./supabase.js";
 
 const sdk = api("@writesonic/v2.2#4enbxztlcbti48j");
 
 export default async function chatSonic(msg: string) {
+  let { data: accounts, error } = await supabase
+    .from("chatsonic")
+    .select("*")
+    .neq("key", null);
+
+  if (!accounts) {
+    return {
+      error: `We are running out of credits, please wait until we solve the issue.`,
+    };
+  }
+  var firstOne = await accounts[0];
+  console.log(firstOne.id);
+  if (!firstOne) {
+    return {
+      error: `We are running out of credits, please wait until we solve the issue.`,
+    };
+  }
+
   sdk.auth(process.env.CHATSONIC);
   try {
     var { data } = await sdk.chatsonic_V2BusinessContentChatsonic_post(
@@ -13,9 +32,15 @@ export default async function chatSonic(msg: string) {
       },
       { engine: "premium" }
     );
-    console.log(data);
     return { text: data.message, type: "chatsonic" };
   } catch (err) {
+    const { data, error } = await supabase
+      .from("accounts")
+      .update({
+        key: null,
+      })
+      .eq("id", firstOne.id);
+
     return { error: err };
   }
 }
