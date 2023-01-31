88ff7dcb0ae198ada1832d0cd8375b98d99328ca
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-01-31 16:04:09 +0100

    fix

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,6 @@
 import { Events, ActivityType } from "discord.js";
 import chalk from "chalk";
-import { reloadTokens, resetto0 } from "../modules/loadbalancer.js";
+import { resetto0 } from "../modules/loadbalancer.js";
 import ms from "ms";
 import supabase from "../modules/supabase.js";
 
@@ -16,13 +16,6 @@ export default {
       status: "online",
     });
 
-    await reloadTokens();
-    /*setInterval(async () => {
-      await reloadConversations();
-    }, ms("1m"));*/
-    setInterval(async () => {
-      await reloadTokens();
-    }, ms("10m"));
     const { data, error } = await supabase.from("conversations").delete();
     console.log(
       chalk.white(`Ready! Logged in as `) + chalk.blue.bold(client.user.tag)
