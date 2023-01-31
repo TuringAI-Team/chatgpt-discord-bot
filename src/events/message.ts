4964d7d35a32a29e616de4c7706b3c51d8d711be
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-01-31 20:27:06 +0100

    fix: ping

---
+++ src/events/message.ts
@@ -0,0 +1,45 @@
+import { Events } from "discord.js";
+import chalk from "chalk";
+import ms from "ms";
+import supabase from "../modules/supabase.js";
+import { isPremium } from "../modules/premium.js";
+import { SlashCommandBuilder, EmbedBuilder, time } from "discord.js";
+
+export default {
+  name: Events.MessageCreate,
+  once: false,
+  async execute(message, client) {
+    if (message.isMemberMentioned(client.user)) {
+      var fields = [];
+      var commands = await client.commands.toJSON();
+      for (var i = 0; i < commands.length; i++) {
+        var command = commands[i].data;
+        var optionMsg = "";
+        var maxLength = command.options.length;
+        if (maxLength > 3) maxLength = 3;
+        for (var j = 0; j < maxLength; j++) {
+          var option = command.options[j];
+          optionMsg += " `" + `${option.name}: ${option.description}` + "`";
+        }
+        var newField = {
+          name: `/${command.name} ${optionMsg}`,
+          value: command.description,
+          inline: false,
+        };
+        fields.push(newField);
+      }
+      var embed = new EmbedBuilder()
+        .setColor("#813479")
+        .setTimestamp()
+        .setTitle("ChatGPT Help")
+        .addFields(fields)
+        .setFooter({
+          text: "This is not an official bot.",
+        });
+      await message.reply({
+        embeds: [embed],
+      });
+      return;
+    }
+  },
+};
