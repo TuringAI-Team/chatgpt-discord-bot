3cb33ee526ac235a4c454ed407ae352b0c766f25
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-02-01 07:07:02 +0100

    fix less cooldown

--- src/commands/chat.ts
+++ src/commands/chat.ts
@@ -12,7 +12,7 @@ import { isPremium } from "../modules/premium.js";
 var maintenance = false;
 
 export default {
-  cooldown: "150s",
+  cooldown: "90s",
   data: new SlashCommandBuilder()
     .setName("chat")
     .setDescription("Chat with an AI")
