ac37331dd0acc496cf37c3be81b523ad7e43f8df
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-02-20 06:50:35 +0100

    feat: imagine

---
+++ src/commands/imagine.ts
@@ -0,0 +1,249 @@
+import {
+  SlashCommandBuilder,
+  AttachmentBuilder,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  StringSelectMenuBuilder,
+} from "discord.js";
+import "dotenv/config";
+import supabase from "../modules/supabase.js";
+import {
+  checkGeneration,
+  generateImg,
+  generateImg2img,
+  png2webp,
+} from "../modules/stablehorde.js";
+import { isPremium } from "../modules/premium.js";
+import { createCanvas, loadImage, Image } from "canvas";
+import sharp from "sharp";
+import { generateRateRow, generateUpscaleRow } from "../modules/stablehorde.js";
+
+var data = new SlashCommandBuilder()
+  .setName("imagine")
+  .setDescription("Generate an image using open joruney.")
+  .addStringOption((option) =>
+    option
+      .setName("prompt")
+      .setDescription("The prompt for generating an image")
+      .setRequired(true)
+  );
+export default {
+  cooldown: "2m",
+  data,
+  async execute(interaction, client) {
+    var tags = [];
+    var guildId;
+    if (interaction.guild) guildId = interaction.guild.id;
+    var ispremium = await isPremium(interaction.user.id, guildId);
+    if (
+      interaction.channel &&
+      interaction.channel.id != "1049275551568896000" &&
+      interaction.channel.id != "1047053103414911026" &&
+      interaction.guild.id == "899761438996963349"
+    ) {
+      interaction.reply({
+        content: `For use this utility go to <#1049275551568896000>`,
+        ephemeral: true,
+      });
+      return;
+    }
+    var steps = 50;
+    if (ispremium) steps = 100;
+
+    var prompt = interaction.options.getString("prompt");
+
+    prompt = `${prompt}, ${tags.join(", ")}`;
+    var defaultNegPrompt = `lowres, bad anatomy, ((bad hands)), (error), ((missing fingers)), extra digit, fewer digits, awkward fingers, cropped, jpeg artifacts, worst quality, low quality, signature, blurry, extra ears, (deformed, disfigured, mutation, extra limbs:1.5),`;
+    var nsfw = false;
+    var FullnegPrompt = defaultNegPrompt;
+
+    if (interaction.channel && interaction.channel.nsfw) nsfw = true;
+    if (!interaction.channel) nsfw = true;
+
+    var width = 512;
+    var height = 512;
+    await interaction.deferReply();
+
+    try {
+      var generation;
+      generation = await generateImg(prompt, steps, nsfw);
+
+      if (generation.message) {
+        if (
+          generation.message.includes(
+            "This prompt appears to violate our terms of service and will be reported. Please contact us if you think this is an error."
+          )
+        ) {
+          const channel = client.channels.cache.get("1055943633716641853");
+          console.log(channel);
+          channel.send(
+            `**Wrong prompt from __${interaction.user.tag}__** (${interaction.user.id})\n**Prompt:** ${prompt}\n**NSFW:** ${nsfw}`
+          );
+        }
+
+        await interaction.editReply({
+          content: `Something wrong happen:\n${generation.message}`,
+          ephemeral: true,
+        });
+        return;
+      }
+    } catch (err) {
+      console.log(err);
+      await interaction.editReply({
+        content: `Something wrong happen.`,
+        ephemeral: true,
+      });
+      return;
+    }
+
+    var interval = setInterval(async () => {
+      try {
+        var status = await checkGeneration(generation);
+        if (status.done) {
+          clearInterval(interval);
+          const { data, error } = await supabase.from("results").insert([
+            {
+              id: generation.id,
+              prompt: prompt,
+              provider: "imagine",
+              result: status.generations,
+              uses: 1,
+            },
+          ]);
+
+          await sendResults(
+            status.generations,
+            interaction,
+            prompt,
+            generation.id,
+            interaction.user.id
+          );
+        } else {
+          if (status.wait_time == undefined) {
+            console.log("No wait time", status);
+            clearInterval(interval);
+            await interaction.editReply({
+              content: `Something wrong happen.`,
+              ephemeral: true,
+            });
+          }
+          try {
+            var waittime = status.wait_time;
+            if (waittime < 15) waittime = 15;
+            await interaction.editReply({
+              content: `Loading...(${waittime}s)`,
+            });
+          } catch (err) {
+            console.log(err);
+            clearInterval(interval);
+            await interaction.editReply({
+              content: `Something wrong happen.`,
+              ephemeral: true,
+            });
+          }
+        }
+      } catch (err) {
+        console.log(err);
+        clearInterval(interval);
+        await interaction.editReply({
+          content: `Something wrong happen.`,
+          ephemeral: true,
+        });
+      }
+    }, 15000);
+  },
+};
+
+async function sendResults(images, interaction, prompt, id: string, userId) {
+  var imagesArr = images.map(async (g, i) => {
+    const sfbuff = Buffer.from(g.img, "base64");
+    var img = await sharp(sfbuff).toFormat("png").toBuffer();
+
+    return new AttachmentBuilder(img, { name: "output.png" });
+  });
+
+  var embed = new EmbedBuilder()
+    .setColor("#347d9c")
+    .setTimestamp()
+    .setImage(`attachment://output.png`)
+    .setFields({
+      name: "Prompt",
+      value: prompt,
+      inline: false,
+    });
+
+  var row = await generateRateRow(id, userId, images[0].id);
+  if (imagesArr.length > 1) {
+    row = await generateUpscaleRow(id, images);
+  }
+  var imgs = images.map((g, i) => {
+    const sfbuff = Buffer.from(g.img, "base64");
+    return sfbuff;
+  });
+
+  let base64: any = await mergeBase64(imgs, 512 / 2, 512 / 2);
+  base64 = base64.split("base64,")[1];
+  var sfbuff = Buffer.from(base64, "base64");
+  var resfile = new AttachmentBuilder(sfbuff, { name: "output.png" });
+  var resfiles = [resfile];
+
+  var reply = await interaction.editReply({
+    files: resfiles,
+    components: row,
+    content: `${interaction.user}`,
+    embeds: [embed],
+  });
+}
+
+async function mergeBase64(imgs: string[], width, height) {
+  var totalW = width * 2;
+  var totalH = height * 2;
+
+  if (imgs.length == 1) {
+    totalW = totalW / 2;
+    totalH = totalH / 2;
+  }
+  if (imgs.length == 2) {
+    totalH = totalH / 2;
+  }
+  var canvas = createCanvas(totalW, totalH);
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < imgs.length; i++) {
+    var im = await sharp(imgs[i]).toFormat("png").toBuffer();
+    var b64 = Buffer.from(im).toString("base64");
+    const img = new Image();
+    var x = 0;
+    var y = 0;
+    if (i == 0) {
+      x = 0;
+      y = 0;
+    }
+    if (i == 1) {
+      x = width;
+      y = 0;
+    }
+    if (i == 2) {
+      x = 0;
+      y = height;
+    }
+    if (i == 3) {
+      x = width;
+      y = height;
+    }
+    img.onload = () => ctx.drawImage(img, x, y, width, height);
+    img.onerror = (err) => {
+      throw err;
+    };
+    img.src = `data:image/png;base64,${b64}`;
+  }
+
+  const dataURL = canvas.toDataURL();
+  return dataURL;
+}
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ src/modules/stablehorde.ts
@@ -0,0 +1,208 @@
+import StableHorde from "@zeldafan0225/stable_horde";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+const stable_horde = new StableHorde({
+  cache_interval: 1000 * 10,
+  cache: {
+    generations_check: 1000 * 30,
+  },
+  client_agent: "Turing-AI-Discord-bot:1.0:(discord)Mrlol#0333",
+  default_token: process.env.STABLE_HORDE,
+});
+import sharp from "sharp";
+import { Configuration, OpenAIApi } from "openai";
+import "dotenv/config";
+import axios from "axios";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default stable_horde;
+export async function getModels() {
+  var models = await stable_horde.getModels();
+  return models;
+}
+
+export async function generateImg(
+  prompt: string,
+  steps: number,
+  nsfw: boolean
+) {
+  var passFilter = await filter(prompt, "Open Journey Beta");
+  if (!passFilter) {
+    return {
+      message:
+        "To prevent generation of unethical images, we cannot allow this prompt with NSFW models/tags.",
+    };
+  }
+
+  const generation = await stable_horde.postAsyncGenerate({
+    prompt: prompt,
+    nsfw: nsfw,
+    censor_nsfw: nsfw == true ? false : true,
+    r2: false,
+    shared: true,
+    models: ["Open Journey Beta"],
+    params: {
+      n: 2,
+      steps: steps,
+      //      width,
+      //      height,
+    },
+  });
+  return generation;
+}
+export async function generateImg2img(
+  prompt: string,
+  model: string,
+  steps: number,
+  amount: number,
+  nsfw: boolean,
+  source_image: string,
+  source_processing: (typeof StableHorde.SourceImageProcessingTypes)[keyof typeof StableHorde.SourceImageProcessingTypes],
+  cfg_scale,
+  sampler,
+  width,
+  height,
+  strength
+) {
+  var passFilter = await filter(prompt, model);
+  if (!passFilter) {
+    return {
+      message:
+        "To prevent generation of unethical images, we cannot allow this prompt with NSFW models/tags.",
+    };
+  }
+
+  const generation = await stable_horde.postAsyncGenerate({
+    prompt: prompt,
+    nsfw: nsfw,
+    censor_nsfw: nsfw == true ? false : true,
+    r2: false,
+    shared: true,
+    models: [model],
+    source_image,
+    source_processing,
+    params: {
+      n: amount,
+      steps: steps,
+      cfg_scale,
+      sampler_name: sampler,
+      width,
+      height,
+      denoising_strength: strength,
+    },
+  });
+  return generation;
+}
+
+export async function png2webp(pngUrl) {
+  const response = await axios.get(pngUrl, { responseType: "arraybuffer" });
+  const imageBuffer = Buffer.from(response.data, "binary");
+  const webpBuffer = await sharp(imageBuffer).toFormat("webp").toBuffer();
+
+  // Convert the WebP image buffer to a base64 string
+  const webpBase64 = webpBuffer.toString("base64");
+
+  return webpBase64;
+}
+
+async function filter(prompt, model?) {
+  var youngWords = [
+    "kid",
+    "kids",
+    "lolis",
+    "children",
+    "child",
+    "boy",
+    "baby",
+    "young",
+    "teen",
+    "teenager",
+    "niñita",
+    "years",
+    "16yo",
+    "year old",
+    "underage",
+    "underaged",
+    "under-age",
+    "under-aged",
+    "juvenile",
+    "minor",
+    "underaged-minor",
+    "youngster",
+    "young teen",
+    "preteen",
+    "pre-teen",
+    "infant",
+    "toddler",
+    "baby",
+    "prepubescent",
+    "minor-aged",
+    "jenna ortega",
+    "hermione",
+  ];
+  var nsfwModels = ["Hentai Diffusion"];
+  var nsfwWords = ["naked", "nude", "uncensored"];
+  var isNsfw = false;
+  var isYoung = false;
+  if (nsfwModels.find((x) => x == model)) isNsfw = true;
+  if (nsfwWords.some((v) => prompt.toLowerCase().includes(v))) isNsfw = true;
+  if (youngWords.some((v) => prompt.toLowerCase().includes(v))) isYoung = true;
+  if (!isYoung) {
+    var result = await openai.createModeration({
+      input: prompt,
+    });
+    isYoung = result.data.results[0].categories["sexual/minors"];
+  }
+  if (isYoung && isNsfw) return false;
+  return true;
+}
+
+export async function checkGeneration(generation: any) {
+  // check the status of your generation using the generations id
+  const check = await stable_horde.getGenerationStatus(generation.id);
+  return check;
+}
+export async function generateUpscaleRow(generationId, images) {
+  const row = new ActionRowBuilder();
+  for (var i = 0; i < images.length; i++) {
+    var btn1 = new ButtonBuilder() //1
+      .setCustomId(`u_${generationId}_${images[i].id}`)
+      .setStyle(ButtonStyle.Secondary)
+      .setLabel(`U${i + 1}`);
+    row.addComponents(btn1);
+  }
+  return [row];
+}
+export async function generateRateRow(generationId, userId, imageId) {
+  const row = new ActionRowBuilder();
+  const btn1 = new ButtonBuilder() //1
+    .setCustomId(`r_${generationId}_${imageId}_${userId}_1`)
+    .setStyle(ButtonStyle.Secondary)
+    .setLabel("😖");
+  row.addComponents(btn1);
+  //\😒  \😀 \😍️️️️️️\☹️
+  const btn2 = new ButtonBuilder() //3
+    .setCustomId(`r_${generationId}_${imageId}_${userId}_3`)
+    .setStyle(ButtonStyle.Secondary)
+    .setLabel("☹️");
+  row.addComponents(btn2);
+  const btn3 = new ButtonBuilder() //5
+    .setCustomId(`r_${generationId}_${imageId}_${userId}_5`)
+    .setStyle(ButtonStyle.Secondary)
+    .setLabel("😒");
+  row.addComponents(btn3);
+  const btn4 = new ButtonBuilder() //7
+    .setCustomId(`r_${generationId}_${imageId}_${userId}_7`)
+    .setStyle(ButtonStyle.Secondary)
+    .setLabel("😀");
+  row.addComponents(btn4);
+  const btn5 = new ButtonBuilder() //9
+    .setCustomId(`r_${generationId}_${imageId}_${userId}_9`)
+    .setStyle(ButtonStyle.Secondary)
+    .setLabel("😍️️️️️️");
+  row.addComponents(btn5);
+  return [row];
+}
