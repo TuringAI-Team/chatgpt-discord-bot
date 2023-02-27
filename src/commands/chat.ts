93e4bda736c7aad052e8c500e99f12829cc56b40
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2023-02-27 22:06:12 +0100

    feat

--- src/commands/chat.ts
+++ src/commands/chat.ts
@@ -269,7 +269,7 @@ async function responseWithText(
     .replaceAll("@everyone", "everyone")
     .replaceAll("@here", "here")
     .replaceAll("<@", "@");
-  if (result.includes("/GENERATE_IMAGE")) {
+  /* if (result.includes("/GENERATE_IMAGE")) {
     var fullprompt = result.split("/GENERATE_IMAGE")[1];
     console.log(fullprompt);
     var p = fullprompt.split("prompt:")[1];
@@ -281,60 +281,60 @@ async function responseWithText(
       await ImagineInteraction(interaction, client, style, p);
       return;
     }
-  } else {
-    var completeResponse = `**${interaction.user.tag}:** ${prompt}\n**AI(${type}):** ${result}`;
-    var charsCount = completeResponse.split("").length;
-    const row = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setStyle(ButtonStyle.Danger)
-        .setLabel(`Reset conversation with ${type}`)
-        .setCustomId(`reset_${type}-${interaction.user.id}`)
-    );
-    var rows = [];
-    if (type != "error") {
-      rows.push(row);
-    }
-    if (image) {
-      var files = [
-        {
-          attachment: image.url,
-          name: "image.png",
-        },
-      ];
-    }
-    if (charsCount / 2000 >= 1) {
-      var lastMsg;
-      var loops = Math.ceil(charsCount / 2000);
-      for (var i = 0; i < loops; i++) {
-        if (i == 0) {
+  } else {*/
+  var completeResponse = `**${interaction.user.tag}:** ${prompt}\n**AI(${type}):** ${result}`;
+  var charsCount = completeResponse.split("").length;
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setStyle(ButtonStyle.Danger)
+      .setLabel(`Reset conversation with ${type}`)
+      .setCustomId(`reset_${type}-${interaction.user.id}`)
+  );
+  var rows = [];
+  if (type != "error") {
+    rows.push(row);
+  }
+  if (image) {
+    var files = [
+      {
+        attachment: image.url,
+        name: "image.png",
+      },
+    ];
+  }
+  if (charsCount / 2000 >= 1) {
+    var lastMsg;
+    var loops = Math.ceil(charsCount / 2000);
+    for (var i = 0; i < loops; i++) {
+      if (i == 0) {
+        try {
+          lastMsg = await commandType.reply(interaction, {
+            content: completeResponse.split("").slice(0, 2000).join(""),
+            components: rows,
+            files: files,
+          });
+        } catch (err) {
+          console.log(err);
+        }
+      } else {
+        if (channel) {
           try {
-            lastMsg = await commandType.reply(interaction, {
-              content: completeResponse.split("").slice(0, 2000).join(""),
-              components: rows,
-              files: files,
-            });
-          } catch (err) {
-            console.log(err);
-          }
-        } else {
-          if (channel) {
-            try {
-              lastMsg = await lastMsg.reply(
-                completeResponse
-                  .split("")
-                  .slice(2000 * i, 2000 * i + 2000)
-                  .join("")
-              );
-            } catch (err) {}
-          }
+            lastMsg = await lastMsg.reply(
+              completeResponse
+                .split("")
+                .slice(2000 * i, 2000 * i + 2000)
+                .join("")
+            );
+          } catch (err) {}
         }
       }
-    } else {
-      commandType.reply(interaction, {
-        content: completeResponse,
-        components: rows,
-        files: files,
-      });
     }
+  } else {
+    commandType.reply(interaction, {
+      content: completeResponse,
+      components: rows,
+      files: files,
+    });
   }
+  //  }
 }
--- src/whatsapp/commands/claim.ts
+++ src/whatsapp/commands/claim.ts
@@ -18,8 +18,5 @@ export default {
   },
 };
 
-/*fffa04bc-fb53-4cea-8fb8-250cf2a050a4
-4d2b7f05-edb2-4310-93dc-91c006a9148b
-0db46493-626e-4ccb-9f80-c4629bb096da
-cc6556f0-1d8a-4343-a2e5-f95b23798988
-8714093f-2b7c-40f6-9675-895aa8d65004*/
+/*
+ */
