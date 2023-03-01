de82c4dffc3832caa0eb56c2ef02c5cf94b4cf2a
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-03-01 19:20:53 +0100

    fix

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -21,12 +21,12 @@ export default {
     console.log(
       chalk.white(`Ready! Logged in as `) + chalk.blue.bold(client.user.tag)
     );
-    if (client.shard.client.options.shards[0] + 1 == 1) {
+    /*if (client.shard.client.options.shards[0] + 1 == 1) {
       await checkLimited();
       setInterval(async () => {
         await checkLimited();
       }, ms("2m"));
-    }
+    }*/
 
     /*
     const { data: tokens } = await supabase
