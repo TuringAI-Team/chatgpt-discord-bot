b8e8dc8bd213b73ad3d453ae56cac9ca664beb5a
Amos Ayomide <amosayomide2004@gmail.com>
2023-03-08 15:58:03 +0100

    Update index.ts

--- src/telegram/index.ts
+++ src/telegram/index.ts
@@ -1,9 +1,13 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
+
+import eventHandler from "./handlers/events.js";
 import commandHandler from "./handlers/commands.js";
 
 var bot: any = new Telegraf(process.env.TELEGRAM);
 bot.commands = [];
-commandHandler(bot);
+
+eventHandler(client);
+commandHandler(client);
 
 bot.launch();
