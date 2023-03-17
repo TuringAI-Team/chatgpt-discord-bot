857449d5e4056c5bc9e3068126fe94b204e7f64c
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-03-17 19:42:17 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,4 @@ const manager = new ShardingManager("./dist/bot.js", {
 manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
 
-import "./whatsapp/index.js";
+//import "./whatsapp/index.js";
