2ea882bb22b0c3238c2e68a4fd44455237078179
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-03-19 10:44:34 +0100

    fix

--- src/modules/open-assistant/langs.ts
+++ src/modules/open-assistant/langs.ts
@@ -1,46 +1,48 @@
 export async function getTranlation(lang: string) {
-  var res = await fetch(
-    `https://open-assistant.io/locales/${lang}/common.json`
-  );
-  var json = await res.json();
-  var res2 = await fetch(
-    `https://open-assistant.io/locales/${lang}/tasks.json`
-  );
-  var json2 = await res2.json();
-  var res3 = await fetch(
-    `https://open-assistant.io/locales/${lang}/dashboard.json`
-  );
-  var json3 = await res3.json();
-  var res4 = await fetch(
-    `https://open-assistant.io/locales/${lang}/leaderboard.json`
-  );
-  var json4 = await res4.json();
-  var res5 = await fetch(
-    `https://open-assistant.io/locales/${lang}/labelling.json`
-  );
-  var json5 = await res5.json();
-  var res6 = await fetch(
-    `https://open-assistant.io/locales/${lang}/message.json`
-  );
-  var json6 = await res6.json();
-  var res7 = await fetch(
-    `https://open-assistant.io/locales/${lang}/index.json`
-  );
-  var json7 = await res7.json();
-  var translationObject = {
-    ...json,
-    ...json2,
-    ...json3,
-    ...json4,
-    ...json5,
-    ...json6,
-    ...json7,
-  };
-  if (!translationObject["skip"]) {
-    var englishTranslation = await getTranlation("en");
-    translationObject["skip"] = englishTranslation["skip"];
-  }
-  return translationObject;
+  try {
+    var res = await fetch(
+      `https://open-assistant.io/locales/${lang}/common.json`
+    );
+    var json = await res.json();
+    var res2 = await fetch(
+      `https://open-assistant.io/locales/${lang}/tasks.json`
+    );
+    var json2 = await res2.json();
+    var res3 = await fetch(
+      `https://open-assistant.io/locales/${lang}/dashboard.json`
+    );
+    var json3 = await res3.json();
+    var res4 = await fetch(
+      `https://open-assistant.io/locales/${lang}/leaderboard.json`
+    );
+    var json4 = await res4.json();
+    var res5 = await fetch(
+      `https://open-assistant.io/locales/${lang}/labelling.json`
+    );
+    var json5 = await res5.json();
+    var res6 = await fetch(
+      `https://open-assistant.io/locales/${lang}/message.json`
+    );
+    var json6 = await res6.json();
+    var res7 = await fetch(
+      `https://open-assistant.io/locales/${lang}/index.json`
+    );
+    var json7 = await res7.json();
+    var translationObject = {
+      ...json,
+      ...json2,
+      ...json3,
+      ...json4,
+      ...json5,
+      ...json6,
+      ...json7,
+    };
+    if (!translationObject["skip"]) {
+      var englishTranslation = await getTranlation("en");
+      translationObject["skip"] = englishTranslation["skip"];
+    }
+    return translationObject;
+  } catch (e) {}
 }
 
 var locales = [
