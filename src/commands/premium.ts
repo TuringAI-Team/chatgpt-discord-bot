2a2f6ef56bb7654a0cc002a54082f3e1af32b248
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-04-08 21:33:04 +0200

    feat: premium info

--- src/commands/premium.ts
+++ src/commands/premium.ts
@@ -9,7 +9,8 @@ import {
   StringSelectMenuBuilder,
 } from "discord.js";
 import "dotenv/config";
-import { activateKey } from "../modules/premium.js";
+import { activateKey, isPremium } from "../modules/premium.js";
+import supabase from "../modules/supabase.js";
 
 var data = new SlashCommandBuilder()
   .setName("premium")
@@ -46,6 +47,11 @@ var data = new SlashCommandBuilder()
             }
           )
       )
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("info")
+      .setDescription("Get information about your subscription.")
   );
 export default {
   data,
@@ -61,7 +67,7 @@ export default {
           "`/premium claim`",
         ephemeral: false,
       });
-    } else {
+    } else if (interaction.options.getSubcommand() === "claim") {
       if (!key) {
         await interaction.editReply({
           content: `Invalid key`,
@@ -98,6 +104,81 @@ export default {
           ephemeral: true,
         });
       }
+    } else if (interaction.options.getSubcommand() === "info") {
+      let guildId;
+      if (interaction.guild) guildId = interaction.guild.id;
+      let premium = await isPremium(interaction.user.id, guildId);
+      if (premium) {
+        let premiumData = await supabase
+          .from("premium")
+          .select("*")
+
+          // Filters
+          .eq("id", interaction.user.id);
+        if (!premiumData.data[0]) {
+          // check if user is admin
+          if (
+            !interaction.member.permissions.has(
+              PermissionsBitField.Flags.Administrator
+            )
+          ) {
+            await interaction.reply({
+              content: `You need to be an administrator to check your server subscription.`,
+              ephemeral: true,
+            });
+            return;
+          }
+          premiumData = await supabase
+            .from("premium")
+            .select("*")
+            .eq("id", interaction.guild.id);
+        }
+        let embed = new EmbedBuilder()
+          .setTitle("Turing AI Premium")
+          .setTimestamp()
+          .addFields(
+            {
+              name: "Type",
+              value: premiumData.data[0].type,
+              inline: true,
+            },
+            {
+              name: "Expires at",
+              value: formatMs(premiumData.data[0].expires_at),
+              inline: true,
+            },
+            {
+              name: "Renewed at",
+              value: formatMs(premiumData.data[0].renewed_at),
+              inline: true,
+            }
+          )
+          .setColor("#5865F2");
+        await interaction.editReply({
+          content: `${interaction.user}, here is your premium information:`,
+          embeds: [embed],
+          ephemeral: true,
+        });
+      } else {
+        await interaction.editReply({
+          content:
+            `You don't have Turing AI Premium. You can buy a key to get Turing AI Premium [here](https://turingai.mysellix.io/). After buying your key you can activate your subscription using the command:` +
+            "`/premium claim`",
+          ephemeral: true,
+        });
+      }
     }
   },
 };
+
+function formatMs(ms) {
+  //  format to dd/mm/yyyy hh:mm:ss
+  var d = new Date(ms);
+  var date = d.getDate();
+  var month = d.getMonth() + 1;
+  var year = d.getFullYear();
+  var hours = d.getHours();
+  var minutes = d.getMinutes();
+  var seconds = d.getSeconds();
+  return `${date}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
