b05baeed00d3f4cae3c93720de8b1b744c3b0049
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-04-11 15:41:47 +0200

    fix: sharding

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import "dotenv/config";
 //import "./telegram/index.js";
 const manager = new ShardingManager("./dist/bot.js", {
   token: process.env.TOKEN,
+  respawn: true,
+  totalShards: "auto",
 });
 
 manager.on("shardCreate", (shard) => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
+manager.spawn({ amount: "auto", timeout: 60000 });
 import "./whatsapp/index.js";
