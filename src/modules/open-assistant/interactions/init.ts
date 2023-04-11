65a546b8a1048a423ef20dee4d868d5fc1081c01
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-04-11 07:56:57 +0200

    fix

--- src/modules/open-assistant/interactions/init.ts
+++ src/modules/open-assistant/interactions/init.ts
@@ -26,22 +26,34 @@ export async function initInteraction(interaction, translation, lang) {
       )} is not complete yet and this can lead to bugs.`
     );
   }
-  const row = new ActionRowBuilder().addComponents(
-    new ButtonBuilder()
-      .setLabel(translation.about)
-      .setCustomId(`oa_info_n_${interaction.user.id}`)
-      .setStyle(ButtonStyle.Primary),
-    new ButtonBuilder()
-      .setLabel(translation.grab_a_task)
-      .setCustomId(`oa_tasks_n_${interaction.user.id}`)
-      .setStyle(ButtonStyle.Primary)
-      .setDisabled(false),
-    new ButtonBuilder()
-      .setLabel("Change language")
-      .setCustomId(`oa_lang-btn_n_${interaction.user.id}`)
-      .setStyle(ButtonStyle.Secondary)
-      .setDisabled(false)
-  );
+
+  var row;
+  if (translation) {
+    row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel(translation.about)
+        .setCustomId(`oa_info_n_${interaction.user.id}`)
+        .setStyle(ButtonStyle.Primary),
+      new ButtonBuilder()
+        .setLabel(translation.grab_a_task)
+        .setCustomId(`oa_tasks_n_${interaction.user.id}`)
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(false),
+      new ButtonBuilder()
+        .setLabel("Change language")
+        .setCustomId(`oa_lang-btn_n_${interaction.user.id}`)
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(false)
+    );
+  } else {
+    row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setLabel("Change language")
+        .setCustomId(`oa_lang-btn_n_${interaction.user.id}`)
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(false)
+    );
+  }
   await interaction.editReply({
     embeds: [embed],
     components: [row],
