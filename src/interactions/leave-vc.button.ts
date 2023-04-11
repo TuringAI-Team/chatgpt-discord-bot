349427395f03751dd55681b52a8cfd92e3a094a4
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-04-11 20:05:52 +0200

    fix

--- src/interactions/leave-vc.button.ts
+++ src/interactions/leave-vc.button.ts
@@ -17,13 +17,44 @@ export default {
         await interaction.deferReply();
       } catch (err) {}
     }
-    if (getVoiceConnection(interaction.guildId)) {
-      var voiceConnection = getVoiceConnection(interaction.guildId);
+    try {
+      if (getVoiceConnection(interaction.guildId)) {
+        var voiceConnection = getVoiceConnection(interaction.guildId);
 
-      var channel = client.channels.cache.get(
-        voiceConnection.joinConfig.channelId
-      );
-      if (!channel) {
+        var channel = client.channels.cache.get(
+          voiceConnection.joinConfig.channelId
+        );
+        if (!channel) {
+          await interaction.editReply({
+            content: "No voice connection was found to this discord channel",
+            components: [],
+            embeds: [],
+            ephemeral: true,
+          });
+        }
+        if (channel.members.has(interaction.user.id)) {
+          getVoiceConnection(interaction.guildId).disconnect();
+          const index = client.guildsVoice.indexOf(interaction.guildId);
+          if (index > -1) {
+            // only splice array when item is found
+            client.guildsVoice.splice(index, 1); // 2nd parameter means remove one item only
+          }
+          await interaction.editReply({
+            content: "ChatGPT voice off",
+            components: [],
+            embeds: [],
+            ephemeral: true,
+          });
+        } else {
+          await interaction.editReply({
+            content:
+              "You are not connnected to the same voice channel as the bot.",
+            components: [],
+            embeds: [],
+            ephemeral: true,
+          });
+        }
+      } else {
         await interaction.editReply({
           content: "No voice connection was found to this discord channel",
           components: [],
@@ -31,35 +62,8 @@ export default {
           ephemeral: true,
         });
       }
-      if (channel.members.has(interaction.user.id)) {
-        getVoiceConnection(interaction.guildId).disconnect();
-        const index = client.guildsVoice.indexOf(interaction.guildId);
-        if (index > -1) {
-          // only splice array when item is found
-          client.guildsVoice.splice(index, 1); // 2nd parameter means remove one item only
-        }
-        await interaction.editReply({
-          content: "ChatGPT voice off",
-          components: [],
-          embeds: [],
-          ephemeral: true,
-        });
-      } else {
-        await interaction.editReply({
-          content:
-            "You are not connnected to the same voice channel as the bot.",
-          components: [],
-          embeds: [],
-          ephemeral: true,
-        });
-      }
-    } else {
-      await interaction.editReply({
-        content: "No voice connection was found to this discord channel",
-        components: [],
-        embeds: [],
-        ephemeral: true,
-      });
+    } catch (e) {
+      console.log(e);
     }
   },
 };
