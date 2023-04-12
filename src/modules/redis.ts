e9ee333bf3400479f67e0486e17dc9481bd46234
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-04-12 18:44:04 +0200

    feat: change redis

--- src/modules/redis.ts
+++ src/modules/redis.ts
@@ -1,7 +1,11 @@
 import { createClient, defineScript } from "redis";
 
 const redisClient = createClient({
-  password: process.env.REDIS_PASSWORD,
+  password: process.env.REDIS_PASSWORD || "password",
+  socket: {
+    host: process.env.REDIS_HOST || "localhost",
+    port: 15266,
+  },
 });
 
 redisClient.on("error", (err) => console.log("Client error: Redis", err));
