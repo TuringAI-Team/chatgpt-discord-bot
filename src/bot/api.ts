a02603bfdc9c287bd7e0e8d96dbedc364e984815
mrloldev <mrlol.yt.oficial@gmail.com>
2023-09-03 20:50:09 +0200

    feat: bot command init version

--- src/bot/api.ts
+++ src/bot/api.ts
@@ -1,26 +1,42 @@
-import { Text, Image } from "turing.sh";
-import { TURING_API_KEY, TURING_CAPTCHA_KEY, TURING_HOST } from "../config.js";
+import { Text, Image, Other } from "turing.sh";
+import {
+  TURING_API_KEY,
+  TURING_CAPTCHA_KEY,
+  TURING_HOST,
+  TURING_SUPER_KEY,
+} from "../config.js";
 
 export function createAPI() {
-	return {
-		text: new Text({
-			apiKey: TURING_API_KEY,
-			captchaKey: TURING_CAPTCHA_KEY,
+  return {
+    text: new Text({
+      apiKey: TURING_API_KEY,
+      captchaKey: TURING_CAPTCHA_KEY,
 
-			options: {
-				host: TURING_HOST,
-				stream: true
-			}
-		}),
+      options: {
+        host: TURING_HOST,
+        stream: true,
+      },
+    }),
 
-		image: new Image({
-			apiKey: TURING_API_KEY,
-			captchaKey: TURING_CAPTCHA_KEY,
+    image: new Image({
+      apiKey: TURING_API_KEY,
+      captchaKey: TURING_CAPTCHA_KEY,
 
-			options: {
-				host: TURING_HOST,
-				stream: true
-			}
-		})
-	};
+      options: {
+        host: TURING_HOST,
+        stream: true,
+      },
+    }),
+
+    other: new Other({
+      apiKey: TURING_API_KEY,
+      captchaKey: TURING_CAPTCHA_KEY,
+      superKey: TURING_SUPER_KEY,
+
+      options: {
+        host: TURING_HOST,
+        stream: true,
+      },
+    }),
+  };
 }
---
+++ src/bot/commands/bot.ts
@@ -0,0 +1,23 @@
+import type { Conversation } from "../types/conversation.js";
+
+import { createCommand } from "../helpers/command.js";
+import { ResponseError } from "../error/response.js";
+import { EmbedColor } from "../utils/response.js";
+
+import { BRANDING_COLOR } from "../../config.js";
+export default createCommand({
+  name: "bot",
+  description: "View information & statistics about the bot",
+
+  handler: async ({ bot, env, interaction }) => {
+    let stats = await bot.api.other.stats();
+    return {
+      embeds: {
+        description: `Bot: ${JSON.stringify(stats)}`,
+        color: BRANDING_COLOR,
+      },
+
+      ephemeral: false,
+    };
+  },
+});
