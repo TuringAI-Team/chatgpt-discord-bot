ad59eb7dcce695824036148b3161daa2993180ba
mrloldev <mrlol.yt.oficial@gmail.com>
2023-09-03 20:55:38 +0200

    fix: bot command

--- src/bot/commands/mod.ts
+++ src/bot/commands/mod.ts
@@ -7,27 +7,27 @@ import Settings from "./settings.js";
 import Imagine from "./imagine.js";
 import Premium from "./premium.js";
 import Reset from "./reset.js";
+import Bot from "./bot.js";
 
-export const COMMANDS: Command[] = [
-	Settings, Reset, Imagine, Premium
-];
+export const COMMANDS: Command[] = [Settings, Reset, Imagine, Premium, Bot];
 
 function transformCommand(command: Command): CreateApplicationCommand {
-	return {
-		name: command.name,
-		description: command.description,
-		type: command.type,
+  return {
+    name: command.name,
+    description: command.description,
+    type: command.type,
 
-		options: command.options ?
-			Object.entries(command.options).map(([ name, data ]) => ({
-				name, ...data
-			}))
-			: undefined
-	};
+    options: command.options
+      ? Object.entries(command.options).map(([name, data]) => ({
+          name,
+          ...data,
+        }))
+      : undefined,
+  };
 }
 
 export async function registerCommands() {
-	bot.helpers.upsertGlobalApplicationCommands(
-		Object.values(COMMANDS).map(c => transformCommand(c))
-	);
+  bot.helpers.upsertGlobalApplicationCommands(
+    Object.values(COMMANDS).map((c) => transformCommand(c))
+  );
 }
