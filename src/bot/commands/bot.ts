d6eaa6496952158ae5919a1c2503600e2318af36
mrloldev <mrlol.yt.oficial@gmail.com>
2023-09-03 21:15:28 +0200

    feat: partners embed

--- src/bot/commands/bot.ts
+++ src/bot/commands/bot.ts
@@ -5,6 +5,23 @@ import { ResponseError } from "../error/response.js";
 import { EmbedColor } from "../utils/response.js";
 
 import { BRANDING_COLOR } from "../../config.js";
+
+const partners = [
+  {
+    emoji: "<:trident:1128664558425362522>",
+    name: "TridentNodes",
+    url: "https://link.turing.sh/tridentnodes",
+    description: "Reliable, powerful, affordable hosting",
+  },
+  {
+    emoji: "<:runpod:1121108621170839592>",
+    name: "RunPod",
+    url: "https://link.turing.sh/runpod",
+    description:
+      "Providing various GPU models for the bot, like image recognition",
+  },
+];
+
 export default createCommand({
   name: "bot",
   description: "View information & statistics about the bot",
@@ -14,18 +31,31 @@ export default createCommand({
     let startDate = new Date("Thu, 15 Dec 2022 18:27:08 UTC");
     let msStart = startDate.getTime();
     return {
-      embeds: {
-        title: "Bot Statistics",
-        description: `Bot: ${JSON.stringify(stats)}`,
-        fields: [
-          {
-            name: "Servers 🖥️",
-            value: `${stats.guilds}`,
-          },
-        ],
-        color: BRANDING_COLOR,
-        timestamp: msStart,
-      },
+      embeds: [
+        {
+          title: "Bot Statistics",
+          fields: [
+            {
+              name: "Servers 🖥️",
+              value: `${stats.guilds}`,
+            },
+          ],
+          color: BRANDING_COLOR,
+          timestamp: msStart,
+        },
+        {
+          color: BRANDING_COLOR,
+          title: "Partners 🤝",
+          description: partners
+            .map(
+              (p) =>
+                `${p.emoji ? `${p.emoji} ` : ""}[**${p.name}**](${p.url})${
+                  p.description ? ` — *${p.description}*` : ""
+                }`
+            )
+            .join("\n"),
+        },
+      ],
 
       ephemeral: false,
     };
