8abcd1d6414bdc4189f2053d428e499b594d972b
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-12-15 22:41:35 +0100

    fix

--- src/bot/utils/conversations.ts
+++ src/bot/utils/conversations.ts
@@ -11,7 +11,6 @@ export async function getConversation(userId: string, modelName: string) {
 	const numberOfUserMessages = conversation.history.messages.filter((x) => x.role === "user").length;
 	const numberOfBotMessages = conversation.history.messages.filter((x) => x.role !== "user").length;
 	// for each 2 messages, there would be 1 bot message
-	console.log(numberOfUserMessages, numberOfBotMessages, numberOfUserMessages > numberOfBotMessages * 2);
 	if (numberOfUserMessages > numberOfBotMessages * 2) {
 		const updatedConversation = {
 			history: {
