d2aac01c16b466fc33967c5d606b707a03671b46
MrlolDev <mrlol.yt.oficial@gmail.com>
2024-03-02 17:15:33 +0100

    Refactor code to improve performance and readability

--- src/bot/commands/chat.ts
+++ src/bot/commands/chat.ts
@@ -1,13 +1,13 @@
 import {
-	BigString,
-	Bot,
-	ButtonComponent,
-	ButtonStyles,
-	Collection,
-	CreateMessageOptions,
-	Message,
-	MessageComponentTypes,
-	delay,
+  BigString,
+  Bot,
+  ButtonComponent,
+  ButtonStyles,
+  Collection,
+  CreateMessageOptions,
+  Message,
+  MessageComponentTypes,
+  delay,
 } from "@discordeno/bot";
 import config from "../../config.js";
 import { NoCooldown, buttonInfo, createCommand } from "../config/setup.js";
@@ -18,258 +18,303 @@ import { env } from "../utils/db.js";
 import { LOADING_INDICATORS } from "../../types/models/users.js";
 import { CHAT_MODELS } from "../models/index.js";
 import EventEmitter from "events";
-import { addMessageToConversation, addMessagesToConversation, getConversation, newConversation } from "../utils/conversations.js";
+import {
+  addMessageToConversation,
+  addMessagesToConversation,
+  getConversation,
+  newConversation,
+} from "../utils/conversations.js";
 import { getDefaultValues, getSettingsValue } from "../utils/settings.js";
 import { chargePlan, requiredPremium } from "../utils/premium.js";
 
 export default createCommand({
-	body: {
-		name: "chat",
-		description: "Chat with the bot",
-		options: [
-			{
-				type: "String",
-				name: "prompt",
-				description: "The prompt that will be used for the text generation",
-				max_length: 1000,
-				required: true,
-			},
-		],
-	},
-	cooldown: {
-		user: 2 * 60 * 1000,
-		voter: 90 * 1000,
-		subscription: 45 * 1000,
-	},
-	interaction: async ({ interaction, options, env, premium }) => {
-		const edit = async (message: CreateMessageOptions) =>
-			await interaction
-				.edit(message)
-				.catch((...args) => ["chat interaction", interaction, ...args].forEach((x) => interaction.bot.logger.warn(x)));
-		await buildInfo(interaction.bot, interaction.user.id, edit, env, interaction.guildId, options, premium);
-	},
-	message: async ({ message, bot, args, env, premium }) => {
-		const parser = {
-			getString: () => args.join(" "),
-		} as unknown as OptionResolver;
-		let previousMsg: Message | undefined;
-		const edit = async (msg: CreateMessageOptions) => {
-			//	console.log(previousMsg ? previousMsg.id : "no previous message");
-			if (previousMsg?.id) {
-				previousMsg = await bot.helpers.editMessage(previousMsg.channelId, previousMsg.id, msg).catch((...args) => {
-					["chat message", previousMsg, ...args].forEach((x) => bot.logger.warn(x));
-					return undefined;
-				});
-			} else {
-				previousMsg = await bot.helpers.sendMessage(message.channelId, {
-					...msg,
-					messageReference: {
-						failIfNotExists: false,
-						messageId: message.id,
-						guildId: message.guildId,
-					},
-					allowedMentions: {
-						repliedUser: true,
-						parse: [],
-					},
-				});
-			}
-		};
-		await buildInfo(bot, message.author.id, edit, env, message.guildId, parser, premium);
-	},
+  body: {
+    name: "chat",
+    description: "Chat with the bot",
+    options: [
+      {
+        type: "String",
+        name: "prompt",
+        description: "The prompt that will be used for the text generation",
+        max_length: 1000,
+        required: true,
+      },
+    ],
+  },
+  cooldown: {
+    user: 2 * 60 * 1000,
+    voter: 90 * 1000,
+    subscription: 45 * 1000,
+  },
+  interaction: async ({ interaction, options, env, premium }) => {
+    const edit = async (message: CreateMessageOptions) =>
+      await interaction
+        .edit(message)
+        .catch((...args) =>
+          ["chat interaction", interaction, ...args].forEach((x) =>
+            interaction.bot.logger.warn(x)
+          )
+        );
+    await buildInfo(
+      interaction.bot,
+      interaction.user.id,
+      edit,
+      env,
+      interaction.guildId,
+      options,
+      premium
+    );
+  },
+  message: async ({ message, bot, args, env, premium }) => {
+    const parser = {
+      getString: () => args.join(" "),
+    } as unknown as OptionResolver;
+    let previousMsg: Message | undefined;
+    const edit = async (msg: CreateMessageOptions) => {
+      //	console.log(previousMsg ? previousMsg.id : "no previous message");
+      if (previousMsg?.id) {
+        previousMsg = await bot.helpers
+          .editMessage(previousMsg.channelId, previousMsg.id, msg)
+          .catch((...args) => {
+            ["chat message", previousMsg, ...args].forEach((x) =>
+              bot.logger.warn(x)
+            );
+            return undefined;
+          });
+      } else {
+        previousMsg = await bot.helpers.sendMessage(message.channelId, {
+          ...msg,
+          messageReference: {
+            failIfNotExists: false,
+            messageId: message.id,
+            guildId: message.guildId,
+          },
+          allowedMentions: {
+            repliedUser: true,
+            parse: [],
+          },
+        });
+      }
+    };
+    await buildInfo(
+      bot,
+      message.author.id,
+      edit,
+      env,
+      message.guildId,
+      parser,
+      premium
+    );
+  },
 });
 
 async function buildInfo(
-	bot: Bot,
-	userId: bigint,
-	edit: (message: CreateMessageOptions) => void,
-	env: Environment,
-	guildId?: BigString,
-	options?: OptionResolver,
-	premium?: {
-		type: "plan" | "subscription";
-		location: "user" | "guild";
-	} | null,
+  bot: Bot,
+  userId: bigint,
+  edit: (message: CreateMessageOptions) => void,
+  env: Environment,
+  guildId?: BigString,
+  options?: OptionResolver,
+  premium?: {
+    type: "plan" | "subscription";
+    location: "user" | "guild";
+  } | null
 ): Promise<void> {
-	//const envrionment = await env(userId.toString(), guildId?.toString());
+  //const envrionment = await env(userId.toString(), guildId?.toString());
 
-	const prompt: string = options?.getString("prompt") ?? "";
-	const user = env.user;
+  const prompt: string = options?.getString("prompt") ?? "";
+  const user = env.user;
 
-	let loadingIndicatorIndex = (await getSettingsValue(user, "general:loadingIndicator")) as number | string;
-	if (
-		!loadingIndicatorIndex ||
-		loadingIndicatorIndex === "default" ||
-		(typeof loadingIndicatorIndex === "string" && parseInt(loadingIndicatorIndex) >= LOADING_INDICATORS.length)
-	) {
-		loadingIndicatorIndex = (await getDefaultValues("general:loadingIndicator")) as number;
-	}
-	const loadingIndicator = LOADING_INDICATORS[loadingIndicatorIndex as number];
-	await edit({
-		content: `<${loadingIndicator.emoji.animated ? "a" : ""}:${loadingIndicator.emoji.name}:${loadingIndicator.emoji.id}>`,
-	});
+  let loadingIndicatorIndex = (await getSettingsValue(
+    user,
+    "general:loadingIndicator"
+  )) as number | string;
+  if (
+    !loadingIndicatorIndex ||
+    loadingIndicatorIndex === "default" ||
+    (typeof loadingIndicatorIndex === "string" &&
+      parseInt(loadingIndicatorIndex) >= LOADING_INDICATORS.length)
+  ) {
+    loadingIndicatorIndex = (await getDefaultValues(
+      "general:loadingIndicator"
+    )) as number;
+  }
+  const loadingIndicator = LOADING_INDICATORS[loadingIndicatorIndex as number];
+  await edit({
+    content: `<${loadingIndicator.emoji.animated ? "a" : ""}:${
+      loadingIndicator.emoji.name
+    }:${loadingIndicator.emoji.id}>`,
+  });
 
-	let setting = (await getSettingsValue(user, "chat:model")) as string;
-	if (!setting) {
-		setting = (await getDefaultValues("chat:model")) as string;
-	}
-	const modelName = setting;
-	const model = CHAT_MODELS.find((x) => x.id === modelName);
-	if (!model) {
-		return await edit({
-			content: "Model not found",
-		});
-	}
-	if (model.premium && !premium) {
-		return await edit(requiredPremium as CreateMessageOptions);
-	}
+  let setting = (await getSettingsValue(user, "chat:model")) as string;
+  if (!setting) {
+    setting = (await getDefaultValues("chat:model")) as string;
+  }
+  const modelName = setting;
+  let model = CHAT_MODELS.find((x) => x.id === modelName);
+  if (!model) {
+    const defaultModel = (await getDefaultValues("chat:model")) as string;
+    model = CHAT_MODELS.find((x) => x.id === defaultModel);
+  }
+  if (!model) {
+    return await edit({
+      content: "An error occurred",
+    });
+  }
+  if (model.premium && !premium) {
+    return await edit(requiredPremium as CreateMessageOptions);
+  }
 
-	let conversation = await getConversation(userId.toString(), modelName);
-	const history = conversation?.history ?? {
-		messages: [],
-	};
-	const data: {
-		messages: { role: string; content: string }[];
-		max_tokens?: number;
-		temperature?: number;
-		model?: string;
-	} = {
-		max_tokens: premium ? 500 : 300,
-		messages: [
-			...history.messages,
-			{
-				role: "user",
-				content: prompt,
-			},
-		],
-	};
-	if (modelName === "gemini") {
-		data.model = "gemini-pro";
-	}
-	try {
-		const event = await model.run(bot.api, data);
+  let conversation = await getConversation(userId.toString(), modelName);
+  const history = conversation?.history ?? {
+    messages: [],
+  };
+  const data: {
+    messages: { role: string; content: string }[];
+    max_tokens?: number;
+    temperature?: number;
+    model?: string;
+  } = {
+    max_tokens: premium ? 500 : 300,
+    messages: [
+      ...history.messages,
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  };
+  if (modelName === "gemini") {
+    data.model = "gemini-pro";
+  }
+  try {
+    const event = await model.run(bot.api, data);
 
-		if (!event || !(event instanceof EventEmitter)) {
-			return await edit({
-				content: "An error occurred",
-			});
-		}
+    if (!event || !(event instanceof EventEmitter)) {
+      return await edit({
+        content: "An error occurred",
+      });
+    }
 
-		let lastUpdate = Date.now();
-		let done = false;
-		event.on("data", async (data) => {
-			if (data.result === "") return;
-			data.result = data.result.replaceAll("@everyone", "everyone").replaceAll("@here", "here");
-			// make a regex to replace all mentions of users or roles
-			data.result = data.result.replaceAll(/<&\d+>/g, "role").replaceAll(/<@\d+>/g, "user");
-			const characters = data.result.split("");
+    let lastUpdate = Date.now();
+    let done = false;
+    event.on("data", async (data) => {
+      if (data.result === "") return;
+      data.result = data.result
+        .replaceAll("@everyone", "everyone")
+        .replaceAll("@here", "here");
+      // make a regex to replace all mentions of users or roles
+      data.result = data.result
+        .replaceAll(/<&\d+>/g, "role")
+        .replaceAll(/<@\d+>/g, "user");
+      const characters = data.result.split("");
 
-			if (!data.done) {
-				if (lastUpdate + 1000 < Date.now() && !done) {
-					// if last update was more than 1 second ago
-					lastUpdate = Date.now();
-					if (characters.length > 2000) {
-						await edit({
-							content: `<${loadingIndicator.emoji.animated ? "a" : ""}:${loadingIndicator.emoji.name}:${
-								loadingIndicator.emoji.id
-							}>`,
-							// send result as a file
-							files: [
-								{
-									name: "chat.txt",
-									blob: new Blob([data.result], { type: "text/plain" }),
-								},
-							],
-						});
-					} else {
-						await edit({
-							content: `${data.result}<${loadingIndicator.emoji.animated ? "a" : ""}:${loadingIndicator.emoji.name}:${
-								loadingIndicator.emoji.id
-							}>`,
-						});
-					}
-				}
-			} else {
-				done = true;
-				if (conversation) {
-					await addMessagesToConversation(conversation, [
-						{
-							role: "user",
-							content: prompt,
-						},
-						{
-							role: "assistant",
-							content: data.result,
-						},
-					]);
-				} else {
-					conversation = await newConversation(
-						[
-							{
-								role: "user",
-								content: prompt,
-							},
-							{
-								role: "assistant",
-								content: data.result,
-							},
-						],
+      if (!data.done) {
+        if (lastUpdate + 1000 < Date.now() && !done) {
+          // if last update was more than 1 second ago
+          lastUpdate = Date.now();
+          if (characters.length > 2000) {
+            await edit({
+              content: `<${loadingIndicator.emoji.animated ? "a" : ""}:${
+                loadingIndicator.emoji.name
+              }:${loadingIndicator.emoji.id}>`,
+              // send result as a file
+              files: [
+                {
+                  name: "chat.txt",
+                  blob: new Blob([data.result], { type: "text/plain" }),
+                },
+              ],
+            });
+          } else {
+            await edit({
+              content: `${data.result}<${
+                loadingIndicator.emoji.animated ? "a" : ""
+              }:${loadingIndicator.emoji.name}:${loadingIndicator.emoji.id}>`,
+            });
+          }
+        }
+      } else {
+        done = true;
+        if (conversation) {
+          await addMessagesToConversation(conversation, [
+            {
+              role: "user",
+              content: prompt,
+            },
+            {
+              role: "assistant",
+              content: data.result,
+            },
+          ]);
+        } else {
+          conversation = await newConversation(
+            [
+              {
+                role: "user",
+                content: prompt,
+              },
+              {
+                role: "assistant",
+                content: data.result,
+              },
+            ],
 
-						userId.toString(),
-						modelName,
-					);
-				}
+            userId.toString(),
+            modelName
+          );
+        }
 
-				// if last update was less than 1 second ago, wait 1 second
-				if (lastUpdate + 1000 > Date.now()) await delay(1000);
-				await chargePlan(data.cost, env, "chat", modelName);
-				let contentR: {
-					content?: string;
-					files?: {
-						name: string;
-						blob: Blob;
-					}[];
-				} = {
-					content: `${data.result}`,
-				};
-				if (characters.length > 2000) {
-					contentR = {
-						content: "",
-						files: [
-							{
-								name: "chat.txt",
-								blob: new Blob([data.result], { type: "text/plain" }),
-							},
-						],
-					};
-				}
-				await edit({
-					...contentR,
-					components: [
-						{
-							type: MessageComponentTypes.ActionRow,
-							components: [
-								{
-									type: MessageComponentTypes.Button,
-									label: model.name,
-									customId: "settings_open",
-									disabled: false,
-									emoji: {
-										name: model.emoji.name,
-										id: BigInt(model.emoji.id),
-									},
-									style: ButtonStyles.Secondary,
-								},
-							],
-						},
-					],
-				});
-			}
-		});
-	} catch (e) {
-		console.log(e);
-		return await edit({
-			content: "An error occurred",
-		});
-	}
+        // if last update was less than 1 second ago, wait 1 second
+        if (lastUpdate + 1000 > Date.now()) await delay(1000);
+        await chargePlan(data.cost, env, "chat", modelName);
+        let contentR: {
+          content?: string;
+          files?: {
+            name: string;
+            blob: Blob;
+          }[];
+        } = {
+          content: `${data.result}`,
+        };
+        if (characters.length > 2000) {
+          contentR = {
+            content: "",
+            files: [
+              {
+                name: "chat.txt",
+                blob: new Blob([data.result], { type: "text/plain" }),
+              },
+            ],
+          };
+        }
+        await edit({
+          ...contentR,
+          components: [
+            {
+              type: MessageComponentTypes.ActionRow,
+              components: [
+                {
+                  type: MessageComponentTypes.Button,
+                  label: model?.name,
+                  customId: "settings_open",
+                  disabled: false,
+                  emoji: {
+                    name: model?.emoji.name,
+                    id: BigInt(model ? model.emoji.id : 1),
+                  },
+                  style: ButtonStyles.Secondary,
+                },
+              ],
+            },
+          ],
+        });
+      }
+    });
+  } catch (e) {
+    console.log(e);
+    return await edit({
+      content: "An error occurred",
+    });
+  }
 }
--- src/bot/models/index.ts
+++ src/bot/models/index.ts
@@ -2,245 +2,261 @@ import EventEmitter from "node:events";
 import { Api } from "../api.js";
 import { Claude, Claude_instant } from "./text/anthropic.js";
 import openchat from "./text/openchat.js";
-import { albedobase, cascade, fustercluck, icbinp, sdxl, turbo } from "./stablehorde.js";
+import {
+  albedobase,
+  cascade,
+  fustercluck,
+  icbinp,
+  sdxl,
+  turbo,
+} from "./stablehorde.js";
 import { Zephyr } from "./text/pawan.js";
 import google from "./text/google.js";
 import groq from "./text/groq.js";
 
 type Prettify<T> = {
-	[K in keyof T]: T[K];
+  [K in keyof T]: T[K];
 } & {};
 
-type UnionFromNumber<T extends number, Tuple extends number[] = []> = Tuple["length"] extends T
-	? Tuple[number] | T
-	: UnionFromNumber<T, [...Tuple, Tuple["length"]]>;
+type UnionFromNumber<
+  T extends number,
+  Tuple extends number[] = [],
+> = Tuple["length"] extends T
+  ? Tuple[number] | T
+  : UnionFromNumber<T, [...Tuple, Tuple["length"]]>;
 
 export type Model = {
-	id: string;
-	name: string;
-	description: string;
-	premium?: boolean;
+  id: string;
+  name: string;
+  description: string;
+  premium?: boolean;
 };
 
 export type ChatModel = Prettify<
-	Model & {
-		emoji: { name: string; id: string };
-		maxTokens: 2048 | 4096 | 8192;
-		premium?: boolean;
-		model?: string;
-	}
+  Model & {
+    emoji: { name: string; id: string };
+    maxTokens: 2048 | 4096 | 8192;
+    premium?: boolean;
+    model?: string;
+  }
 >;
 
 export type GPTModel = Prettify<
-	ChatModel & {
-		run: (
-			api: Api,
-			data: {
-				messages: { role: string; content: string }[];
-				max_tokens?: number;
-				temperature?: number;
-				plugins?: string[];
-			},
-		) => EventEmitter | NonNullable<unknown>;
-	}
+  ChatModel & {
+    run: (
+      api: Api,
+      data: {
+        messages: { role: string; content: string }[];
+        max_tokens?: number;
+        temperature?: number;
+        plugins?: string[];
+      }
+    ) => EventEmitter | NonNullable<unknown>;
+  }
 >;
 
 export type AnthropicModel = Prettify<
-	ChatModel & {
-		run: (
-			api: Api,
-			data: {
-				messages: { role: string; content: string }[];
-				max_tokens?: number;
-				temperature?: number;
-			},
-		) => EventEmitter | NonNullable<unknown>;
-	}
+  ChatModel & {
+    run: (
+      api: Api,
+      data: {
+        messages: { role: string; content: string }[];
+        max_tokens?: number;
+        temperature?: number;
+      }
+    ) => EventEmitter | NonNullable<unknown>;
+  }
 >;
 
 export type OpenChatModel = Prettify<
-	ChatModel & {
-		run: (
-			api: Api,
-			data: {
-				messages: { role: string; content: string }[];
-				max_tokens?: number;
-				temperature?: number;
-				model?: string;
-			},
-		) => Promise<EventEmitter | NonNullable<unknown>>;
-	}
+  ChatModel & {
+    run: (
+      api: Api,
+      data: {
+        messages: { role: string; content: string }[];
+        max_tokens?: number;
+        temperature?: number;
+        model?: string;
+      }
+    ) => Promise<EventEmitter | NonNullable<unknown>>;
+  }
 >;
 export type PawanChatModel = Prettify<
-	ChatModel & {
-		run: (
-			api: Api,
-			data: {
-				messages: { role: string; content: string }[];
-				max_tokens?: number;
-				temperature?: number;
-				model?: string;
-			},
-		) => Promise<EventEmitter | NonNullable<unknown>>;
-	}
+  ChatModel & {
+    run: (
+      api: Api,
+      data: {
+        messages: { role: string; content: string }[];
+        max_tokens?: number;
+        temperature?: number;
+        model?: string;
+      }
+    ) => Promise<EventEmitter | NonNullable<unknown>>;
+  }
 >;
 
 export const CHAT_MODELS: (GPTModel | AnthropicModel | OpenChatModel)[] = [
-	/*GPT4, GPT3_5, GPT16K, */
-	groq,
-	openchat,
-	Claude,
-	Claude_instant,
-	Zephyr,
-	google,
+  /*GPT4, GPT3_5, GPT16K, */
+  groq,
+  Claude,
+  Claude_instant,
+  Zephyr,
+  google,
 ];
 export type GenericParam = Parameters<Api["image"]["sh"]>[0];
 
 export const IMAGE_MODELS: (GenericModel<GenericParam> | DALLEModel<2>)[] = [
-	sdxl,
-	albedobase,
-	icbinp,
-	turbo,
-	fustercluck,
-	//OpenJourneyDiffussion,
-	//Deliberate,
-	//majicMIXR,
-	//DALLE3,
-	//kandinsky,
+  sdxl,
+  albedobase,
+  icbinp,
+  turbo,
+  fustercluck,
+  //OpenJourneyDiffussion,
+  //Deliberate,
+  //majicMIXR,
+  //DALLE3,
+  //kandinsky,
 ];
 export type ImageModel = Pick<Model, "name" | "id" | "premium">;
 
 export type ImageModelFixed = Prettify<
-	ImageModel & {
-		fixedSize: { width: number; height: number };
-		baseSize?: never;
-		from?: never;
-		to?: never;
-		variableSizes?: never;
-	}
+  ImageModel & {
+    fixedSize: { width: number; height: number };
+    baseSize?: never;
+    from?: never;
+    to?: never;
+    variableSizes?: never;
+  }
 >;
 
 export type ImageModelBase = Prettify<
-	ImageModel & {
-		fixedSize?: never;
-		baseSize: { width: number; height: number };
-		from?: never;
-		to?: never;
-		variableSizes?: never;
-	}
+  ImageModel & {
+    fixedSize?: never;
+    baseSize: { width: number; height: number };
+    from?: never;
+    to?: never;
+    variableSizes?: never;
+  }
 >;
 
 export type ImageModelFromTo = Prettify<
-	ImageModel & {
-		fixedSize?: never;
-		baseSize?: never;
-		from: { width: number; height: number };
-		to: { width: number; height: number };
-		variableSizes?: never;
-	}
+  ImageModel & {
+    fixedSize?: never;
+    baseSize?: never;
+    from: { width: number; height: number };
+    to: { width: number; height: number };
+    variableSizes?: never;
+  }
 >;
 
 export type ImageModelVariable<T extends number> = Prettify<
-	ImageModel & {
-		fixedSize?: never;
-		baseSize?: never;
-		from?: never;
-		to?: never;
-		variableSizes: {
-			[K in UnionFromNumber<T>]: {
-				width: number;
-				height: number;
-			};
-		};
-	}
+  ImageModel & {
+    fixedSize?: never;
+    baseSize?: never;
+    from?: never;
+    to?: never;
+    variableSizes: {
+      [K in UnionFromNumber<T>]: {
+        width: number;
+        height: number;
+      };
+    };
+  }
 >;
 
 export type ImageModelNone = Prettify<
-	ImageModel & {
-		fixedSize?: never;
-		baseSize?: never;
-		from?: never;
-		to?: never;
-		variableSizes?: never;
-	}
+  ImageModel & {
+    fixedSize?: never;
+    baseSize?: never;
+    from?: never;
+    to?: never;
+    variableSizes?: never;
+  }
 >;
 
 export type DALLEModel<T extends number> = Prettify<
-	ImageModelVariable<T> & {
-		run: (
-			api: Api,
-			data: {
-				prompt: string;
-				number: 1 | 2 | 3 | 4;
-				size?: "256x256" | "512x512" | "1024x1024";
-				image?: string;
-			},
-		) => EventEmitter | NonNullable<unknown>;
-	}
+  ImageModelVariable<T> & {
+    run: (
+      api: Api,
+      data: {
+        prompt: string;
+        number: 1 | 2 | 3 | 4;
+        size?: "256x256" | "512x512" | "1024x1024";
+        image?: string;
+      }
+    ) => EventEmitter | NonNullable<unknown>;
+  }
 >;
 
 export type KandinskyModel = Prettify<
-	ImageModelFromTo & {
-		run: (
-			api: Api,
-			data: {
-				prompt: string;
-				steps?: number;
-				number?: number;
-				negative_prompt?: string;
-				guidance_scale?: number;
-				width?: number;
-				height?: number;
-				cfg_scale?: number;
-				model_version?: "2.1" | "2.2";
-			},
-		) => EventEmitter | NonNullable<unknown>;
-	}
+  ImageModelFromTo & {
+    run: (
+      api: Api,
+      data: {
+        prompt: string;
+        steps?: number;
+        number?: number;
+        negative_prompt?: string;
+        guidance_scale?: number;
+        width?: number;
+        height?: number;
+        cfg_scale?: number;
+        model_version?: "2.1" | "2.2";
+      }
+    ) => EventEmitter | NonNullable<unknown>;
+  }
 >;
 
 export type GenericModel<T extends NonNullable<unknown>> = Prettify<
-	ImageModelFromTo & { run: (api: Api, data: T) => EventEmitter | unknown }
+  ImageModelFromTo & { run: (api: Api, data: T) => EventEmitter | unknown }
 >;
 
 export type ControlNetModel = Prettify<
-	ImageModelNone & {
-		run: (
-			api: Api,
-			data: {
-				prompt: string;
-				image: string;
-				model: "normal" | "canny" | "hough" | "hed" | "depth2img" | "pose" | "seg";
-			},
-		) => EventEmitter | NonNullable<unknown>;
-	}
+  ImageModelNone & {
+    run: (
+      api: Api,
+      data: {
+        prompt: string;
+        image: string;
+        model:
+          | "normal"
+          | "canny"
+          | "hough"
+          | "hed"
+          | "depth2img"
+          | "pose"
+          | "seg";
+      }
+    ) => EventEmitter | NonNullable<unknown>;
+  }
 >;
 
 export type UpscalerModel = Prettify<
-	ImageModelNone & {
-		run: (
-			api: Api,
-			data: {
-				image: string;
-				upscaler?:
-					| "GFPGAN"
-					| "RealESRGAN_x4plus"
-					| "RealESRGAN_x2plus"
-					| "RealESRGAN_x4plus_anime_6B"
-					| "NMKD_Siax"
-					| "4x_AnimeSharp";
-			},
-		) => EventEmitter | NonNullable<unknown>;
-	}
+  ImageModelNone & {
+    run: (
+      api: Api,
+      data: {
+        image: string;
+        upscaler?:
+          | "GFPGAN"
+          | "RealESRGAN_x4plus"
+          | "RealESRGAN_x2plus"
+          | "RealESRGAN_x4plus_anime_6B"
+          | "NMKD_Siax"
+          | "4x_AnimeSharp";
+      }
+    ) => EventEmitter | NonNullable<unknown>;
+  }
 >;
 
 export type ImageVisionModel = Prettify<
-	ImageModelNone & {
-		run: (
-			api: Api,
-			data: {
-				image: string;
-				typeImage: "anything" | "person";
-			},
-		) => EventEmitter | NonNullable<unknown>;
-	}
+  ImageModelNone & {
+    run: (
+      api: Api,
+      data: {
+        image: string;
+        typeImage: "anything" | "person";
+      }
+    ) => EventEmitter | NonNullable<unknown>;
+  }
 >;
